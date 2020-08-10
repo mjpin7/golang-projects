aeea013d162f9d755b50069dd35383ef2bbe7697
MJ Pinheiro <mjpin7@gmail.com>
2020-08-09 20:19:48 -0400

    Reverted test file

--- test.go
+++ test.go
@@ -2,23 +2,8 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	t1 := time.NewTimer(3 * time.Second)
-
-problem:
-	for i := 0; i < 20; i++ {
-		select {
-		case <-t1.C:
-			break problem
-		default:
-			fmt.Printf("%d\n", i)
-			time.Sleep(1 * time.Second)
-		}
-	}
-
-	fmt.Printf("test\n")
-
+	fmt.Printf("Hello World!\n")
 }
